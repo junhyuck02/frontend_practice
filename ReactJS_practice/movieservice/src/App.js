1cc666b9f20eb27e1f62d67fb00dc21806fd0236
최준혁 <ryan020820@gmail.com>
2026-06-05 15:33:48 +0900

    Create App.js

---
+++ ReactJS_practice/movieservice/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {/* swtich -> 한번에 route 하나만 렌더링 되길 원할 때 사용 */}
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
