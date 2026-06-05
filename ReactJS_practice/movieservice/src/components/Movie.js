24e93e467e757fd8d12618fb176532aae19fb82e
최준혁 <ryan020820@gmail.com>
2026-06-05 17:40:36 +0900

    Update Movie.js

--- ReactJS_practice/movieservice/src/components/Movie.js
+++ ReactJS_practice/movieservice/src/components/Movie.js
@@ -1,28 +1,41 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({
+  id,
+  coverImg,
+  title,
+  summary,
+  director,
+  releaseDate,
+  rtScore,
+}) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to="/movie">{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>
+          {releaseDate}년 · RT {rtScore}점
+        </h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <p className={styles.movie__director}>감독: {director}</p>
+      </div>
     </div>
   );
 }
 
 Movie.propTypes = {
+  id: PropTypes.string.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  director: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  rtScore: PropTypes.string.isRequired,
 };
 
 export default Movie;
