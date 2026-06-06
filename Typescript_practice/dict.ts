b38c5b6e9d8b9c166c5a4567da27851e77f3a5ed
최준혁 <ryan020820@gmail.com>
2026-06-06 20:12:06 +0900

    Update dict.ts

--- Typescript_practice/dict.ts
+++ Typescript_practice/dict.ts
@@ -5,7 +5,7 @@ type Words = {
 class Dict {
   private words: Words;
   constructor() {
-    // property가 바로 생성자로부터 초기화 되는게 아니라 수동으로 시켜주기
+    // property가 바로 생성자로부터 초기화 되는게 아니라 수동으로 시켜주는 방법임
     this.words = {};
   }
   // 클래스 안에서는 그냥 바로 함수 이름 쓰면 됨, java랑 같음
