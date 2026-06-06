1264cab55592d7ff3e3094ef8f717ca1c6568ab7
최준혁 <ryan020820@gmail.com>
2026-06-06 17:26:35 +0900

    Create dict.ts

---
+++ Typescript_practice/dict.ts
@@ -0,0 +1,32 @@
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words;
+  constructor() {
+    // property가 바로 생성자로부터 초기화 되는게 아니라 수동으로 시켜주기
+    this.words = {};
+  }
+  // 클래스 안에서는 그냥 바로 함수 이름 쓰면 됨, java랑 같음
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+}
+
+class Word {
+  constructor(
+    public term: string,
+    public def: string,
+  ) {}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+const dict = new Dict();
+dict.add(kimchi);
+dict.def("kimchi");
