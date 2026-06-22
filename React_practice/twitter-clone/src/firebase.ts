46331fd82a21c9d1e65c5d33ad7a5cbf3043793b
최준혁 <ryan020820@gmail.com>
2026-06-22 18:12:06 +0900

    Update firebase.ts

--- React_practice/twitter-clone/src/firebase.ts
+++ React_practice/twitter-clone/src/firebase.ts
@@ -1,6 +1,8 @@
 // 리액트 프로젝트와 Firebase 서비스를 연결하는 초기 설정 코드
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,3 +17,8 @@ export const app = initializeApp(firebaseConfig);
 
 // 나 authenticaton 이용할거야
 export const auth = getAuth(app);
+
+// 이미지나 동영상, 파일 등을 저장하거나 불러올 때 사용하는 스토리지 이용할거야
+export const storage = getStorage(app);
+// 나 데이터베이스 이용할거야
+export const db = getFirestore(app);
